fbe3a0dd698e7a0d285cc457ba08cafd2ba249ad
Simonvb0 <simon.van.baelen@gmail.com>
2022-10-18 11:14:58 +0200

    Added a better template for the evolutionary algorithm

--- r0123456.py
+++ r0123456.py
@@ -1,5 +1,9 @@
 import Reporter
 import numpy as np
+from numpy import random
+from numpy import ndarray
+import random as rn
+import copy
 
 # Modify the class name to match your student number.
 class r0123456:
@@ -34,3 +38,58 @@ class r0123456:
 
 		# Your code here.
 		return 0
+
+
+
+class TravellingSalesPersonProblem:
+    def __init__(self, numObjects):
+		pass
+		#TODO
+
+
+def fitness(ksp, ind):
+	pass
+	# TODO
+
+def selection(ksp, population):
+	pass
+	# TODO
+
+def recombination(ksp, p1, p2):
+	pass
+	# TODO
+
+def mutate(ksp, ind):
+	pass
+	# TODO
+
+def elimination(kps, population, offspring, mu):
+	pass
+	# TODO
+
+def evolutionaryAlgorithm(ksp):
+    lam = 100
+    mu = 100
+    its = 100
+    # population = [TODO for _ in range(0, lam)]
+
+    for i in range(0,its):
+        offspring = []
+        for j in range(0,mu):
+			# Selection step
+            p1 = selection(ksp, population)
+            p2 = selection(ksp, population)
+
+			#Recombination step
+            offspring.append(recombination(ksp, p1, p2))
+
+			# Mutation step
+            mutate(ksp, offspring[j])
+        for ind in population:
+
+			# Mutation step
+            mutate(ksp, ind)
+
+		# Elimination step
+        population = elimination(ksp, population, offspring, mu)
+
