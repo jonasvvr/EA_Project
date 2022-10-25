2d9a3684db667d5687d9e1d3b4ff36cc4b105443
Simonvb0 <simon.van.baelen@gmail.com>
2022-10-25 12:39:48 +0200

    Added the cycle-function

--- r0123456.py
+++ r0123456.py
@@ -1,5 +1,6 @@
 import Reporter
 import numpy as np
+import sys
 from numpy import random
 from numpy import ndarray
 import random as rn
@@ -41,10 +42,10 @@ class r0123456:
 
 
 
-class TravellingSalesPersonProblem:
-    def __init__(self, numObjects):
-		pass
-		#TODO
+# class TravellingSalesPersonProblem:
+#     def __init__(self, numObjects):
+# 		pass
+# 		#TODO
 
 
 def fitness(ksp, ind):
@@ -86,10 +87,48 @@ def evolutionaryAlgorithm(ksp):
 			# Mutation step
             mutate(ksp, offspring[j])
         for ind in population:
-
 			# Mutation step
             mutate(ksp, ind)
 
 		# Elimination step
         population = elimination(ksp, population, offspring, mu)
 
+def createRandomCycle(a, b, dm, pi):
+	if len(pi) > 0:
+		possibleIndices = pi.copy()
+		toBeAddedLater = []
+		while len(possibleIndices) > 0:
+			j = rn.choice(possibleIndices)
+			possibleIndices.remove(j)
+			toBeAddedLater.append(j)
+			while dm[b][j] == np.inf:
+				if len(possibleIndices) <= 0:
+					return None
+				j = rn.choice(possibleIndices)
+				possibleIndices.remove(j)
+				toBeAddedLater.append(j)
+			possibleIndices = possibleIndices + toBeAddedLater
+			possibleIndices.remove(j)
+			path = createRandomCycle(a, j, dm, possibleIndices)
+			if path is not None:
+				path.append(j)
+				return path
+	else:
+		if dm[a][b] != np.inf:
+			return []
+		else:
+			return None
+
+filename = "tour50.csv"
+file = open(filename)
+distanceMatrix = np.loadtxt(file, delimiter=",")
+file.close()
+
+sys.setrecursionlimit(100000)
+for i in range(0,1000):
+	start = rn.randint(0,len(distanceMatrix)-1)
+	possibleIndices = list(range(0,len(distanceMatrix)))
+	possibleIndices.remove(start)
+	randomCycle = createRandomCycle(start, start, distanceMatrix, possibleIndices)
+	randomCycle.append(start)
+	print(i)
